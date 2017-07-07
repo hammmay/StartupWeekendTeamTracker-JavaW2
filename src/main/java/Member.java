e15cee22d804f1dcacb88429be2bd2c175fe9b1f
Laura Hamilton <laurahamilton9@gmail.com>
2017-07-07 11:46:32 -0700

    Add test and array and method to list multiple new members

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
   private String mMemberName;
-
+  private static List<Member> instancesOfMembers = new ArrayList<Member>();
+//method to instantiate member object
   public Member(String memberName) {
     mMemberName = memberName;
+    instancesOfMembers.add(this);
   }
-
+//method to get name of member object
   public String getMemberName() {
     return mMemberName;
+  }
+//method to get all names of all member objects added to the member array
+  public static List<Member> all() {
+    return instancesOfMembers;
   }
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -2,17 +2,27 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class MemberTest {
-
+//test to instantiate member object
   @Test
   public void Member_instantiatesCorrectly_true() {
     Member myMember = new Member("Human");
     assertEquals(true, myMember instanceof Member);
   }
-
+//test to get name of member object
   @Test
   public void Member_instantiatesWithMemberName_String() {
     Member myMember = new Member("Human");
     assertEquals("Human", myMember.getMemberName());
   }
+//test to retrieve a list of all member names
+  @Test
+  public void all_returnsAllInstancesOfMember_true() {
+    Member firstMember = new Member("Human1");
+    Member secondMember = new Member("Human2");
+    assertEquals(true, Member.all().contains(firstMember));
+    assertEquals(true, Member.all().contains(secondMember));
+  }
+
+
 
 }
